5fb76ebe31131719d7883bf3d680384926b2f202
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-04-29 16:51:24 -0300

    Path Parameters

--- books.py
+++ books.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
 
-@app.get("/api-endpoint")
-async def root():
-    return {"message": "Hello Marshal"}
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/book/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
