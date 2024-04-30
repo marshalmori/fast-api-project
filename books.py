72eb1ab16f0d6db225b4752d743f708c8c2f63f8
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-04-30 15:42:03 -0300

    Query parameters

--- books.py
+++ books.py
@@ -22,3 +22,23 @@ async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
+
+
+# a barra no final de books representa que vai receber uma query
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
